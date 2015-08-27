3d9a5a47746fb83d40e21d4bfb3f3ea0dbf7dbfa
Franck KALB <fkalb@e3r4p23.42.fr>
2015-08-27 11:54:11 +0200

    add strclen

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,20 +6,25 @@
 /*   By: fkalb <fkalb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/19 11:13:03 by fkalb             #+#    #+#             */
-/*   Updated: 2014/01/24 10:52:04 by fkalb            ###   ########.fr       */
+/*   Updated: 2015/08/27 11:53:02 by fkalb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include "libft.h"
 
 size_t		ft_strlen(const char *s)
 {
-	size_t	i;
+	return (ft_strclen(s, '\0'));
+}
+
+size_t		ft_strclen(char const *s, char c)
+{
+	size_t		i;
 
 	i = 0;
 	if (!s)
 		return (0);
-	while (s[i])
+	while (s[i] && s[i] != c)
 		i++;
 	return (i);
 }
